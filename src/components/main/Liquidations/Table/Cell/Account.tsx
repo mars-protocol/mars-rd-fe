ae58b8bdc400e4c221ced612b318c850f1bdf5c9
Patricie <patricie.sm@gmail.com>
2024-08-21 11:45:48 +0200

    feat: added external link to liquidations account id

--- src/components/main/Liquidations/Table/Cell/Account.tsx
+++ src/components/main/Liquidations/Table/Cell/Account.tsx
@@ -1,5 +1,6 @@
 import { ExternalLink } from 'components/common/Icons'
 import Text from 'components/common/Text'
+import { TextLink } from 'components/common/TextLink'
 
 interface Props {
   value: string
@@ -7,10 +8,19 @@ interface Props {
 
 export default function Account(props: Props) {
   const { value } = props
+  const url = `https://app.marsprotocol.io/wallets/osmosiswallet/portfolio${value}`
+
   return (
-    <div className='flex items-center justify-end space-x-1'>
+    <div
+      className='flex items-center justify-end space-x-1'
+      onClick={(e) => {
+        e.stopPropagation()
+      }}
+    >
       <Text size='sm'>#{value}</Text>
-      <ExternalLink className='w-3.5 h-3.5 text-white/40 hover:text-inherit hover:cursor-pointer' />
+      <TextLink href={url} target='_blank' title={`Mars Protocol Account ID #${value}`}>
+        <ExternalLink className='w-3.5 h-3.5 text-white/40 hover:text-inherit hover:cursor-pointer' />
+      </TextLink>
     </div>
   )
 }
