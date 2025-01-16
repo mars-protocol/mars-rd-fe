fdf0089a894742e37622c99d3a408b3b52fe50f6
Monkmansteve <sxatzakis@yahoo.gr>
2025-01-16 15:48:55 +0200

    fix: update layout component to correctly handle iframe view

--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -63,7 +63,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <main
         className={classNames(
           'md:min-h-[calc(100dvh-81px)]',
-          isIframeView ? '' : 'mt-[73px]',
+          !isIframeView && 'mt-[73px]',
           'flex',
           'min-h-screen-full w-full relative',
           'gap-4 p-2 pb-20',
