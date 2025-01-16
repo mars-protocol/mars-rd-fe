be8c15306e690789572db0022f3bcdd9088d043d
Monkmansteve <sxatzakis@yahoo.gr>
2025-01-16 15:58:42 +0200

    refactor: remove assetSelect dropdown in iframe

--- src/components/common/Chart/common/SelectionControlPanel/index.tsx
+++ src/components/common/Chart/common/SelectionControlPanel/index.tsx
@@ -3,9 +3,9 @@ import TimeframeSelector from 'components/common/Chart/common/SelectionControlPa
 import Select from 'components/common/Select'
 
 interface Props {
-  selectOptions: { value: string; label: React.ReactNode }[]
-  defaultSelectValue: string
-  onSelectChange: (value: string) => void
+  selectOptions?: { value: string; label: React.ReactNode }[]
+  defaultSelectValue?: string
+  onSelectChange?: (value: string) => void
   timeframe: TimeframeOption[]
   selectedTimeframe: string
   onTimeframeSelect: (timeframe: string) => void
@@ -22,18 +22,20 @@ export default function SelectionControlPanel(props: Props) {
   } = props
   return (
     <div className='flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between w-full'>
-      <Select
-        options={selectOptions}
-        defaultValue={defaultSelectValue}
-        onChange={onSelectChange}
-        className='relative border w-full sm:w-60 rounded-base border-white/10 bg-white/10'
-        containerClassName='justify-center w-full sm:w-auto'
-      />
+      {selectOptions && onSelectChange && defaultSelectValue && (
+        <Select
+          options={selectOptions}
+          defaultValue={defaultSelectValue}
+          onChange={onSelectChange}
+          className='relative border w-full sm:w-60 rounded-base border-white/10 bg-white/10'
+          containerClassName='justify-center w-full sm:w-auto'
+        />
+      )}
       <TimeframeSelector
         timeframe={timeframe}
         selectedTimeframe={selectedTimeframe}
         setSelectedTimeframe={onTimeframeSelect}
-        className='ml-auto'
+        className={!selectOptions ? 'ml-0' : 'ml-auto'}
       />
     </div>
   )
