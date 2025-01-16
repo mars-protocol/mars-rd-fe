6500749e5f3ca4c8c2d2e44a5a05e1b1f48a4058
Monkmansteve <sxatzakis@yahoo.gr>
2025-01-16 15:41:45 +0200

    feature: add support for iframe view in layout component

--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -14,6 +14,7 @@ import useChainConfig from 'hooks/chain/useChainConfig'
 import useCurrentChainId from 'hooks/localStorage/useCurrentChainId'
 import useStore from 'store'
 import { debugSWR } from 'utils/middleware'
+import { useRouter } from 'next/router'
 
 interface Props {
   focusComponent: FocusComponent | null
@@ -51,6 +52,46 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       useStore.setState({ chainConfig: chains[currentChainId] })
     }
   }, [chainConfig.id, currentChainId, setCurrentChainId])
+  const router = useRouter()
+  const isIframeView = router.query.iframeView === 'on'
+
+  if (isIframeView) {
+    return (
+      <SWRConfig value={{ use: [debugSWR] }}>
+        <PageMetadata />
+        <main
+          className={classNames(
+            'md:min-h-[calc(100dvh-81px)]',
+            'mt-[73px]',
+            'flex',
+            'min-h-screen-full w-full relative',
+            'gap-4 p-2 pb-20',
+            'md:gap-6 md:px-4 md:py-6',
+            'justify-center',
+            focusComponent && 'items-center',
+            isMobile && 'items-start transition-all duration-500',
+            mobileNavExpanded && isMobile && '-ml-full',
+          )}
+        >
+          <Suspense
+            fallback={
+              <div className='flex items-center justify-center w-full h-screen-full'>
+                <div className='flex flex-wrap justify-center w-full gap-4'>
+                  <CircularProgress size={60} />
+                  <Text className='w-full text-center' size='2xl'>
+                    Fetching on-chain data...
+                  </Text>
+                </div>
+              </div>
+            }
+          >
+            <PageContainer focusComponent={focusComponent}>{children}</PageContainer>
+          </Suspense>
+        </main>
+        <Footer />
+      </SWRConfig>
+    )
+  }
 
   return (
     <SWRConfig value={{ use: [debugSWR] }}>
