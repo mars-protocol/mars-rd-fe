05c83f587c00e22bd2c8d3e0a9bf9c38cc7d02f4
Linkie Link <linkielink.dev@gmail.com>
2025-01-16 09:21:43 +0100

    fix: update footer

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -12,7 +12,7 @@ export default function Footer() {
       <div className='w-full p-2 pt-0 text-right md:p-4'>
         <TextLink
           className='text-xs text-white opacity-50 hover:text-white hover:opacity-80'
-          href={`${DocURL.CHANGE_LOG_URL}#release-v${flatVersion}`}
+          href={`${DocURL.FEATURE_URL}${version}`}
           target='_blank'
           title={`Mars Protocol ${version} change log`}
         >
