0a7690afe8a9bd1900fcab64206d74ff91857055
Patricie29 <patricie@marsprotocol.foundation>
2025-06-16 13:15:16 +0700

    fix: remove disabled perps markets

--- src/hooks/assets/usePerpsEnabledAssets.ts
+++ src/hooks/assets/usePerpsEnabledAssets.ts
@@ -4,5 +4,8 @@ import { useMemo } from 'react'
 export default function usePerpsEnabledAssets() {
   const { data: assets } = useAssets()
 
-  return useMemo(() => assets.filter((asset) => asset.isPerpsEnabled), [assets])
+  return useMemo(
+    () => assets.filter((asset) => asset.isPerpsEnabled && !asset.isDeprecated),
+    [assets],
+  )
 }
